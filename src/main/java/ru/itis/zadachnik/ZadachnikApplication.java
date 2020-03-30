018995baa25a773bd522147a5c7d2b2861780623
Kamila Nigmetzianova <kamila-nigmet@mail.ru>
2020-03-31 00:07:03 +0300

    Initial commit

---
+++ src/main/java/ru/itis/zadachnik/ZadachnikApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.zadachnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadachnikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadachnikApplication.class, args);
+    }
+
+}
